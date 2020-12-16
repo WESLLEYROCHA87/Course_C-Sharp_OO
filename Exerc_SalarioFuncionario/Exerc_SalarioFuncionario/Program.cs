7f734efde7727c78fba398140ecec8deeb22047c
Weslley Rocha <weslley.rocha87@gmail.com>
2020-12-16 14:40:43 -0300

    Inserting Statement

--- Exerc_SalarioFuncionario/Exerc_SalarioFuncionario/Program.cs
+++ Exerc_SalarioFuncionario/Exerc_SalarioFuncionario/Program.cs
@@ -8,8 +8,18 @@ namespace Exerc_SalarioFuncionario
     {
         static void Main(string[] args)
         {
+            /*
+            Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto). Em seguida, mostrar os dados do
+            funcionário (nome e salário líquido). Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada 
+            (somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário. 
+
+            Make a program to read an employee's data (name, gross salary and tax). Then show the data of the employee 
+            (name and net salary). Then, increase the employee's salary based on a percentage given
+            (only the gross salary is affected by the percentage) and show employee data again. 
+            */
+
             Employee f = new Employee();
-            
+
             Console.Write("Name: ");
             f.Name = Console.ReadLine();
             Console.Write("Gross Salary: ");
