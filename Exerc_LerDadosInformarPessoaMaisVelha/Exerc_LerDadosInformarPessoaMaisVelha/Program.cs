ab74e46e1c20c693c218675edaa57c4ef892d64a
Weslley Rocha <weslley.rocha87@gmail.com>
2020-12-16 10:22:29 -0300

    Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.

---
+++ Exerc_LerDadosInformarPessoaMaisVelha/Exerc_LerDadosInformarPessoaMaisVelha/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Exerc_LerDadosInformarPessoaMaisVelha.Entities;
+
+namespace Exerc_LerDadosInformarPessoaMaisVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.
+
+            Person p1, p2;
+
+            p1 = new Person();
+            p2 = new Person();
+
+            Console.WriteLine("First person data: ");
+            Console.Write("Name: ");
+            p1.Name = Console.ReadLine();
+            Console.Write("Age: ");
+            p1.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Second person data: ");
+            Console.Write("Name: ");
+            p2.Name = Console.ReadLine();
+            Console.Write("Age: ");
+            p2.Age = int.Parse(Console.ReadLine());
+
+            if (p1.Age > p2.Age)
+            {
+                Console.WriteLine("Older person: " + p1.Name);
+            }
+            else
+            {
+                Console.WriteLine("Older person: " + p2.Name);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
