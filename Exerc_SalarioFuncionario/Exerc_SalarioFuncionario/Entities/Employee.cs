b47511b4c36bdc42cd61f166a1285cd6a361cb83
Weslley Rocha <weslley.rocha87@gmail.com>
2020-12-16 14:33:36 -0300

    Exerc_SalarioFuncionario

---
+++ Exerc_SalarioFuncionario/Exerc_SalarioFuncionario/Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Exerc_SalarioFuncionario.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double GrossSalary { get; set; }
+        public double Tax { get; set; }
+
+        public double LiquidSalary()
+        {
+            return GrossSalary - Tax;
+        }
+
+        public void IncreaseSalary(double percent)
+        {
+            GrossSalary += GrossSalary * percent / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return "Employee: "
+                   + Name + ", $ "
+                   + LiquidSalary().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exerc_SalarioFuncionario/Exerc_SalarioFuncionario/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using Exerc_SalarioFuncionario.Entities;
+
+namespace Exerc_SalarioFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee f = new Employee();
+            
+            Console.Write("Name: ");
+            f.Name = Console.ReadLine();
+            Console.Write("Gross Salary: ");
+            f.GrossSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Tax: ");
+            f.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine(f);
+            Console.WriteLine();
+
+            Console.Write("Enter the percentage to increase the salary: ");
+            double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.IncreaseSalary(percentage);
+
+            Console.WriteLine();
+            Console.WriteLine(f);
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
