5eb171bb630287a80943d58328ab82dee92074b5
Weslley Rocha <weslley.rocha87@gmail.com>
2020-12-16 12:33:25 -0300

    Criando um método para obtermos os benefícios de reaproveitamento e delegação.

--- CalcLadoTrianguloMaior/CalcLadoTrianguloMaior/Entities/Triangulo.cs
+++ CalcLadoTrianguloMaior/CalcLadoTrianguloMaior/Entities/Triangulo.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CalcLadoTrianguloMaior.Entities
 {
@@ -11,5 +7,11 @@ namespace CalcLadoTrianguloMaior.Entities
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));            
+        }
     }
 }
