3fb9d5cfb66aec0d5b42f52a786d24257795773c
Weslley Rocha <weslley.rocha87@gmail.com>
2020-12-16 13:07:56 -0300

    Exerc_Estoque (Classe, Objeto, Método e ToString)

---
+++ Exerc_Estoque/Exerc_Estoque/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Exerc_Estoque.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public double TotalValueInStock()
+        {
+            return Price * Quantity;
+        }
+
+        public void AddProduct(int quantity)
+        {
+            Quantity += quantity;
+        }
+
+        public void RemoveProduct(int quantity)
+        {
+            Quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return "Product data: " + Name + ", $ " 
+                                    + Price.ToString("F2", CultureInfo.InvariantCulture) + ", " 
+                                    + Quantity + " unidades, Total: " 
+                                    + TotalValueInStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Exerc_Estoque/Exerc_Estoque/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using Exerc_Estoque.Entities;
+
+namespace Exerc_Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product();
+
+            Console.WriteLine("Enter product data: ");
+            Console.Write("Name: ");
+            p1.Name = Console.ReadLine();
+            Console.Write("Price: ");
+            p1.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantity: ");
+            p1.Quantity = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine();
+            Console.WriteLine(p1);
+            Console.WriteLine();
+
+            Console.Write("Enter the product number to be added to stock: ");
+            int quantity = int.Parse(Console.ReadLine());
+            p1.AddProduct(quantity);
+
+            Console.WriteLine();
+            Console.WriteLine(p1);
+            Console.WriteLine();
+
+            Console.Write("Enter the product number to be removed stock: ");
+            quantity = int.Parse(Console.ReadLine());
+            p1.RemoveProduct(quantity);
+
+            Console.WriteLine();
+            Console.WriteLine(p1);
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
