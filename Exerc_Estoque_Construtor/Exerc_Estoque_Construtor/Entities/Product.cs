9dfcedc60063446609b23d166c551d502056fead
Weslley Rocha <weslley.rocha87@gmail.com>
2020-12-17 15:51:35 -0300

    Utilizado Construtor

---
+++ Exerc_Estoque_Construtor/Exerc_Estoque_Construtor/Entities/Product.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace Exerc_Estoque_Construtor.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Price * Quantity;
+        }
+
+        public void AdicionarProdutos(int quantity)
+        {
+            Quantity += quantity;
+        }
+
+        public void RemoverProdutos(int quantity)
+        {
+            Quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return "Dados do produto: "
+                + Name + ", $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture) + ", "
+                + Quantity + " unidades, $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exerc_Estoque_Construtor/Exerc_Estoque_Construtor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using Exerc_Estoque_Construtor.Entities;
+
+namespace Exerc_Estoque_Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Product p = new Product(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine(p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qtde);
+
+            Console.WriteLine();
+            Console.WriteLine(p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtde);
+
+            Console.WriteLine();
+            Console.WriteLine(p);
+            Console.WriteLine();            
+
+            Console.ReadKey();
+        }
+    }
+}
