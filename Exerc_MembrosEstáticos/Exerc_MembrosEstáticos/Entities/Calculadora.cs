9f8a1ee95dceae79f26c1b00003bed4f7922a5f3
Weslley Rocha <weslley.rocha87@gmail.com>
2020-12-17 10:03:48 -0300

    Versão 2: classe Calculadora com membros de instância

---
+++ Exerc_MembrosEstáticos/Exerc_MembrosEstáticos/Entities/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc_MembrosEstáticos.Entities
+{
+    class Calculadora
+    {
+        public double Pi = 3.14;
+
+        public double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
--- Exerc_MembrosEstáticos/Exerc_MembrosEstáticos/Program.cs
+++ Exerc_MembrosEstáticos/Exerc_MembrosEstáticos/Program.cs
@@ -1,45 +1,29 @@
 ﻿using System;
 using System.Globalization;
+using Exerc_MembrosEstáticos.Entities;
 
 namespace Exerc_MembrosEstáticos
 {
     class Program
     {
-        static double Pi = 3.14;
-
         static void Main(string[] args)
         {
+            // • Versão 2: classe Calculadora com membros de instância
 
-            /*
-             * Fazer um programa para ler um valor numérico qualquer, e daí mostrar quanto seria o valor de uma circunferência e do volume de uma esfera
-             * para um raio daquele valor. Informar também o valor de PI com duas casas decimais. 
-             
-             * Versão 1: métodos na própria classe do programa 
-             * • Nota: dentro de um método estático você não pode chamar membros de instância da mesma classe.              
-             */
+            Calculadora calc = new Calculadora();
 
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferecia(raio);
-            double volume = Volume(raio);
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
 
             Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de Pi: " + Pi.ToString("F2", CultureInfo.InvariantCulture));            
-            
+            Console.WriteLine("Valor de Pi: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
             Console.ReadKey();
 
-        }
-
-        static double Circunferecia(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * r * r * r;
         }
 
     }
