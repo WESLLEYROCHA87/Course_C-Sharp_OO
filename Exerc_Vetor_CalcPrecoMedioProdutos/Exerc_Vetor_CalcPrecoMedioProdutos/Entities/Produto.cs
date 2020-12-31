029445bb723de48a86f8e97bbf5834383085a0ba
Weslley Rocha <weslley.rocha87@gmail.com>
2020-12-31 15:03:29 -0300

    Vetores com Objeto tipo Referência

---
+++ Exerc_Vetor_CalcPrecoMedioProdutos/Exerc_Vetor_CalcPrecoMedioProdutos/Entities/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc_Vetor_CalcPrecoMedioProdutos.Entities
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ Exerc_Vetor_CalcPrecoMedioProdutos/Exerc_Vetor_CalcPrecoMedioProdutos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Exerc_Vetor_CalcPrecoMedioProdutos.Entities;
+using System.Globalization;
+
+namespace Exerc_Vetor_CalcPrecoMedioProdutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a quantidades de produtos que deseja inserir: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] p = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Entre com o nome do produto: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Entre com o preço do produto: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                p[i] = new Produto { Nome = nome, Preco = preco };
+            }
+
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += p[i].Preco;
+            }
+
+            double media = sum / n;
+
+            Console.Write("Preço Médio = : " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
