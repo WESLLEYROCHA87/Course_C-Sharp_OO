f21695082f453131b2a7a891bb196c742c628e56
Weslley Rocha <weslley.rocha87@gmail.com>
2020-12-31 17:10:20 -0300

    Modificador_params

---
+++ Modificador_params/Modificador_params/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modificador_params.Entities;
+
+namespace Modificador_params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculadora.Soma(5, 5, 5);
+
+            Console.WriteLine(s1);
+
+            Console.ReadKey();
+        }
+    }
+}
